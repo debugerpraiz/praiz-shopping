45d7b0be9400b677298a2f69ed897b0a016be622
debugerpraiz <151669906+debugerpraiz@users.noreply.github.com>
2025-07-29 08:59:12 -1100

    Delete praiz directory

--- praiz/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "", "ecommerce");
-
-if ($mysqli->connect_error) {
-    die("❌ Connection failed: " . $mysqli->connect_error);
-}
-echo "✅ Connected to database!";
-?>
